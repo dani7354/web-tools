41e2d6c0dd65cdf46e5a79c977886bf8febabe33
Daniel <d@stuhrs.dk>
2024-09-17 20:45:21 +0200

    Add argument to rss_media_dl.py

--- web_tools/rss_media_dl.py
+++ web_tools/rss_media_dl.py
@@ -5,6 +5,7 @@ import queue
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from threading import Thread
+from typing import Sequence
 
 import requests
 from slugify import slugify
@@ -16,11 +17,20 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Download media files from RSS feed")
     parser.add_argument("-u", "--url", dest="url", type=str, required=True)
     parser.add_argument("-d", "--dir", dest="dir", type=str, required=True)
+    parser.add_argument("-w", "--words", dest="words", nargs="+", type=str, required=False)
 
     return parser.parse_args()
 
+def filter_rss_items(item: tuple[str, str], words: Sequence[str]) -> bool:
+    url, title = item
+    for word in words:
+        if word in title or word in url:
+            return True
 
-def read_rss_feed(url: str, directory: str) -> list[tuple[str, str]]:
+    return False
+
+
+def read_rss_feed(url: str, directory: str, words: Sequence[str]) -> list[tuple[str, str]]:
     response = requests.get(url)
     response.raise_for_status()
 
@@ -30,8 +40,11 @@ def read_rss_feed(url: str, directory: str) -> list[tuple[str, str]]:
 
     rss_tree = ET.ElementTree(ET.fromstring(response.content))
     items = rss_tree.findall(".//item")
+    item_tuples = filter(
+        lambda x: filter_rss_items(x, words),
+        [(x.find("enclosure").get("url"), x.findtext("title")) for x in items])
 
-    return [(x.find("enclosure").get("url"), x.findtext("title")) for x in items]
+    return list(item_tuples)
 
 
 def create_queue(items) -> queue.Queue:
@@ -66,7 +79,7 @@ def main() -> None:
     args = parse_arguments()
 
     print(f"Reading RSS feed at {args.url}...")
-    items = read_rss_feed(args.url, args.dir)
+    items = read_rss_feed(args.url, args.dir, args.words)
     if not items:
         print("No items found in RSS feed")
         return
